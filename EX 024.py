efe8c35bd8ffc7c476149c97f6c395f080206e7e
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-02-18 20:37:29 -0300

    Adicionando exercicio 24

---
+++ EX 024.py
@@ -0,0 +1,3 @@
+cidade = str(input("Digite o nome da cidade em que voce nasceu: ")).upper().strip()
+print(f"Voce nasceu em  {cidade}")
+print(f"Tem santo no nome da cidade em que voce nasceu ? {'SANTO' in cidade}")
